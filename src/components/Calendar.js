cbbbddfaf929e7582bced7c012f4ed6ccad07ba6
Heckfy <rr.akhmetoff@gmail.com>
2022-12-08 19:15:54 +0000

    Поправил замечание

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -56,27 +56,9 @@ function Calendar({date}) {
         </colgroup>
         <thead>
           <tr>
-            <th scope="col" title="Понедельник">
-              Пн
-            </th>
-            <th scope="col" title="Вторник">
-              Вт
-            </th>
-            <th scope="col" title="Среда">
-              Ср
-            </th>
-            <th scope="col" title="Четверг">
-              Чт
-            </th>
-            <th scope="col" title="Пятница">
-              Пт
-            </th>
-            <th scope="col" title="Суббота">
-              Сб
-            </th>
-            <th scope="col" title="Воскресенье">
-              Вс
-            </th>
+            {['Пн', 'В', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'].map((dayOfWeek) => (
+              <th scope="col">{dayOfWeek}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
